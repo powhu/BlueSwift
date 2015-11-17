345d0b01b456200e0abaa67222f46c60ad7f7bcc
Yang <yang@electrodream.jp>
2015-11-17 14:24:21 +0900

    Initial Commit

---
+++ BlueSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueSwift
+//
+//  Created by Yang on 2015/11/17.
+//  Copyright © 2015年 Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
